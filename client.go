b6b519d49aab3f6db848f3d763d8627b354b7342
Charles Shu <shubo83@vip.qq.com>
2022-08-25 16:40:20 +0800

    解决在nginx反向代理方式 访问web-tail服务时会报request origin not allowed by Upgrader.CheckOrigin错误的问题

--- client.go
+++ client.go
@@ -24,6 +24,10 @@ const (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	// 解决跨域问题
+        CheckOrigin: func(r *http.Request) bool {
+            return true
+        },
 }
 
 type Client struct {
